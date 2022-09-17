8a2cbc35e9f559e6213fb731cd498c0dd77601f2
lukaszkukla <lukaszkukla@gmail.com>
2022-09-17 17:09:12 +0000

    Feat: add and migrate product and category model to admin

--- products/models.py
+++ products/models.py
@@ -1,3 +1,42 @@
+from hashlib import blake2b
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category class
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        Returns friendly name
+        """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Product class
+    """
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    img_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
