9202795cd42fa5b5278d2bbcd81cc613a4eb85bc
lukaszkukla <lukaszkukla@gmail.com>
2022-10-01 20:35:10 +0000

    Feat: finish add product functionality

--- products/views.py
+++ products/views.py
@@ -3,16 +3,13 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-from products.forms import ProductForm
-
 from .models import Product, Category
 from .forms import ProductForm
 
+# Create your views here.
 
 def all_products(request):
-    """
-    A view to show all products, including sorting and search queries
-    """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -29,13 +26,12 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -44,14 +40,10 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(
-                    request,
-                    'You did not enter any search criteria!'
-                )
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -67,9 +59,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """
-    A view to show individual product details
-    """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
@@ -79,13 +69,24 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 def add_product(request):
-    """Add a product to the store"""
-    form = ProductForm()
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
-
+    
