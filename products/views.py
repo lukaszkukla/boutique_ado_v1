18670f3149238f2714a0e18f2e67ed3df8906377
lukaszkukla <lukaszkukla@gmail.com>
2022-10-02 19:45:48 +0000

    Feat: add product custom image control widget

--- products/views.py
+++ products/views.py
@@ -121,6 +121,7 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
+
 @login_required
 def delete_product(request, product_id):
     """Delete product form the store"""
