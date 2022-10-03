4f911c47b30bc61671753706e9a7aec794978479
lukaszkukla <lukaszkukla@gmail.com>
2022-10-03 23:02:16 +0000

    Fix: allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://lukaszkukla-boutique-ado.herokuapp.com', 'localhost', 'http://127.0.0.1:8001/' ]
+ALLOWED_HOSTS = ['https://lukaszkukla-boutique-ado.herokuapp.com', 'localhost', 'http://127.0.0.1:8001/']
 
 # Application definition
 
