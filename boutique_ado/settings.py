9c691d8be81a6e7d4e2e55c9c51f905bad1b8ad6
lukaszkukla <lukaszkukla@gmail.com>
2022-10-05 00:45:00 +0000

    Fix: gmail real emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -209,9 +209,9 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
-if 'DEVELOPMNET' in os.environ:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'lukaszkukladev@gmail.com'
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
